d17812e6d2ea42edcf5536f59c543c93c833c475
b2un0 <branko.wilhelm@gmail.com>
2014-11-04 14:03:48 +0100

    - installer script

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
 /**
- * Template - pwniez
- *
- * @author     Branko Wilhelm <bw@z-index.net>
+ * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
  * @link       http://www.z-index.net
- * @copyright  (c) 2013 Branko Wilhelm
- * @license    GNU General Public License v3
+ * @copyright  (c) 2013 - 2014 Branko Wilhelm
+ * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 
 defined('_JEXEC') or die;
